0d2f7dea47c09e0b1b59bb01c8b71c251ec1d69d
Eric McCarthy <eric@limulus.net>
2014-09-29 22:17:00 -0700

    Copying files from tucsonjs-logo repository.

---
+++ index.js
@@ -0,0 +1,7 @@
+"use strict"
+
+module.exports.SVGRoot = require("./lib/SVGRoot.js")
+module.exports.SVGRect = require("./lib/SVGRect.js")
+module.exports.SVGPolygon = require("./lib/SVGPolygon.js")
+module.exports.SVGCircle = require("./lib/SVGCircle.js")
+module.exports.SVGPath = require("./lib/SVGPath.js")
